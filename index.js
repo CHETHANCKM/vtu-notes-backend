cd381035aa77bc246596ad521b40a02541daf380
Chethan S <cchethans14@gmail.com>
2022-07-31 08:47:55 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const port= 8080;
+
+app.get('/', (req, res)=>{
+    res.send("<!DOCTYPE html><html><body><h1>Hello World!</h1></body></html>")
+    });
+
+app.listen(port, ()=>{
+    console.log(`Server is running at ${port}`)
+});
